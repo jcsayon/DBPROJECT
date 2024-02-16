0f65460b3d3bc9caaf6b53e67712b67e58bbe009
jcsayon <jcsayon@addu.edu.ph>
2024-02-16 14:52:03 +0800

    Change Password

--- frmMain.cs
+++ frmMain.cs
@@ -57,5 +57,24 @@ namespace DBPROJECT
             this.glSetSizeToDesktop();
             this.BringToFront();
         }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ChangePasswordfrm_FormClosed(object sender,FormClosedEventArgs e)
+        {
+            ChangePasswordfrm = null;
+        }
+        private frmChangePassword ChangePasswordfrm;
+
+        private void changePasswordToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChangePasswordfrm = new frmChangePassword(Globals.gIdUser, Globals.gLoginName);
+            ChangePasswordfrm.FormClosed += ChangePasswordfrm_FormClosed;
+
+            ChangePasswordfrm.ShowDialog();
+        }
     }
 }
